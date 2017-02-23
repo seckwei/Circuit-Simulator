cf3284797f5774202202d5351b259085db8d45e6
Seck Wei <seckwei93@gmail.com>
2017-02-23 13:59:21 +0000

    Changing Matrix back to a class that has an array field. Matrix tests passed.

--- spec/Matrix.spec.js
+++ spec/Matrix.spec.js
@@ -7,7 +7,7 @@ describe('Matrix module', () => {
         it('should create []', () => {
             let mat = new Matrix();
             
-            expect(mat.length).toBe(0);
+            expect(mat.data).toEqual([]);
         });
 
     });
@@ -16,14 +16,14 @@ describe('Matrix module', () => {
         it('should create a number*number matrix', () => {
             let actual = new Matrix(5);
 
-            expect(actual.length).toBe(5);
-            expect(actual.every(row => row.length === 5)).toBe(true);    
+            expect(actual.data.length).toBe(5);
+            expect(actual.data.every(row => row.length === 5)).toBe(true);    
         });
         
         it('should create a 0 filled matrix', () => {
             let actual = new Matrix(4);
 
-            expect(actual.every(row => row.every(cell => cell === 0))).toBe(true);
+            expect(actual.data.every(row => row.every(cell => cell === 0))).toBe(true);
         });
     });
 
@@ -31,11 +31,11 @@ describe('Matrix module', () => {
        it('should create a row*col matrix', () => {
            let actual = new Matrix(7,2);
 
-           expect(actual.length).toBe(7);
-           expect(actual.every(row => row.length == 2)).toBe(true);
+           expect(actual.data.length).toBe(7);
+           expect(actual.data.every(row => row.length == 2)).toBe(true);
        });
     });
-    
+            
     describe('addRow(number)', () => {
 
         let mat;
@@ -47,27 +47,27 @@ describe('Matrix module', () => {
             let mat2 = new Matrix();
             mat2.addRow();
 
-            expect(mat2.length).toBe(1);
-            expect(mat2[0]).toEqual([]);
+            expect(mat2.data.length).toBe(1);
+            expect(mat2.data[0]).toEqual([]);
         }); 
 
         it('should append 1 new row by default - no argument passed', () => {
             mat.addRow();
 
-            expect(mat.length).toBe(6);
+            expect(mat.data.length).toBe(6);
         });
         
         it('should append new number of rows as specified by the argument', () => {
             mat.addRow(2);
 
-            expect(mat.length).toBe(7);
+            expect(mat.data.length).toBe(7);
         });
 
         it('should 0-fill the new rows', () => {
             mat.addRow(2);
 
-            expect(mat[5]).toEqual(Array(5).fill(0));
-            expect(mat[6]).toEqual(Array(5).fill(0));
+            expect(mat.data[5]).toEqual(Array(5).fill(0));
+            expect(mat.data[6]).toEqual(Array(5).fill(0));
         });
 
     });
@@ -83,26 +83,26 @@ describe('Matrix module', () => {
             let mat2 = new Matrix();
             mat2.addCol();
 
-            expect(mat2.length).toBe(1);
-            expect(mat2[0]).toEqual([]);
+            expect(mat2.data.length).toBe(1);
+            expect(mat2.data[0]).toEqual([]);
         });
 
         it('should append 1 new col by default - no argument passed', () => {
             mat.addCol();
 
-            expect(mat.every(row => row.length === 6)).toBe(true);
+            expect(mat.data.every(row => row.length === 6)).toBe(true);
         });
         
         it('should append new number of cols as specified by the argument', () => {
             mat.addCol(2);
 
-            expect(mat.every(row => row.length === 7)).toBe(true);
+            expect(mat.data.every(row => row.length === 7)).toBe(true);
         });
 
         it('should 0-fill the new cols', () => {
             mat.addCol(2);
 
-            expect(mat.every(row => row.slice(-2).toString() === '0,0')).toBe(true);
+            expect(mat.data.every(row => row.slice(-2).toString() === '0,0')).toBe(true);
         });
         
     });
@@ -116,5 +116,21 @@ describe('Matrix module', () => {
             expect(Matrix.rowToCol(row)).toEqual(col);
         });
 
+    });
+    
+    describe('itself', () => {
+        
+        it('should be modifiable like a 2D Array', () => {
+            let mat = new Matrix(3);
+            mat.data[0] = [1,2,3]
+            mat.data[1][0] = 3;
+
+            expect(mat.data).toEqual([
+                [1,2,3],
+                [3,0,0],
+                [0,0,0]
+            ]);
+        });
+            
     });
 });
--- src/Matrix.js
+++ src/Matrix.js
@@ -2,75 +2,114 @@
 
 /**
  * @class Matrix
- * @extends Array
  * 
- * The prototypical method is employed rather than ES6 class because
- * inheriting primitive type such as Array will present problems when
- * transpiled. The problem I had is that the super class array intance
- * will always be returned, rather than the derived's instance.
+ * Instead of inheriting from Array type, I wrapped an
+ * array in Matrix class. Array inheritance is still not
+ * supported properly in transpilation. The problem I had 
+ * is that the inherited Array will always be returned,
+ * rather than the derived's instance.
  * See: https://babeljs.io/docs/usage/caveats/
  */
 
-/**
- * Creates an instance of Matrix.
- * @param {(number|void)} [row=undefined] 
- * @param {(number|void)} [col=undefined] 
- * 
- * @memberOf Matrix
- */
-function Matrix(row: number|void = undefined, col: number|void = undefined) {
+class Matrix {
 
-    if(Number.isInteger(row)) {
-        this.length = row;
+    /**
+     * @prop data
+     * @type {number[][]}
+     * @memberOf Matrix
+     */
+    data: number[][];
 
-        if(Number.isInteger(col) || col === undefined || col === null) {
-            let colArr = new Array(col || row).fill(0);
-            this.fill().forEach((r, i, a) => {
-                a[i] = colArr.slice();
+    /**
+     * Creates an instance of Matrix.
+     * @param {number} [row=undefined] 
+     * @param {number} [col=undefined] 
+     * 
+     * @memberOf Matrix
+     */
+    constructor(row: ?number = undefined, col: ?number = undefined) {
+        this.data = [];
+
+        if(Number.isInteger(row)) {
+
+            this.data.length = row;
+
+            if(Number.isInteger(col) || col === undefined || col === null) {
+
+                let colArr = new Array(col || row).fill(0);
+
+                this.data.fill().forEach((r, i, a) => {
+                    a[i] = (colArr.slice());
+                });
+
+            }
+        }
+    }
+
+    /**
+     * @method addRow
+     * 
+     * @param {number} [amount]
+     * 
+     * @memberOf Matrix
+     */
+    addRow(amount: ?number): void {
+        // If this matrix has at least one row
+        if(this.data.length > 0) {
+            // Then we will base the length of the first row
+            // to create the new ones
+            let len = this.data[0].length,
+                arr = [];
+
+            arr.length = len;
+            arr.fill(0);
+
+            // If amount not set, then run once
+            for(let i = 0; i < (Number.isInteger(amount)? amount : 1); ++i) {
+                this.data.push(arr.slice()); // slice called to copy array
+            }
+        }
+        else {
+            for(let i = 0; i < (Number.isInteger(amount)? amount : 1); ++i) {
+                this.data.push([]);
+            }
+        }
+    }
+
+    /**
+     * @method addCol
+     * 
+     * @param {number} [amount]
+     * 
+     * @memberOf Matrix
+     */
+    addCol(amount: ?number): void {
+        // If this matrix has at least one row
+        if(this.data.length > 0) {
+            let newCols = [0];
+            if(Number.isInteger(amount) && amount > 0) {
+                newCols.length = amount;
+                newCols.fill(0);
+            }
+            this.data.map(function(row) {
+                row.push(...newCols);
             });
         }
-    }
-}
-
-Matrix.prototype = Object.create(Array.prototype);
-
-Matrix.prototype.addRow = function (amount: ?number) {
-    // If this matrix has at least one row
-    if(this.length > 0) {
-        // Then we will base the length of the first row
-        // to create the new ones
-        let len = this[0].length,
-            arr = [];
-
-        arr.length = len;
-        arr.fill(0);
-
-        // If amount not set, then run once
-        for(let i = 0; i < (Number.isInteger(amount)? amount : 1); ++i) {
-            this.push(arr.slice()); // slice called to copy array
+        else {
+            this.data.push([]);
         }
     }
-    else {
-        for(let i = 0; i < (Number.isInteger(amount)? amount : 1); ++i) {
-            this.push([]);
-        }
-    }
-};
 
-Matrix.prototype.addCol = function (amount: ?number) {
-    // If this matrix has at least one row
-    if(this.length > 0) {
-        let newCols = [0];
-        if(Number.isInteger(amount) && amount > 0) {
-            newCols.length = amount;
-            newCols.fill(0);
-        }
-        this.map(function(row) {
-            row.push(...newCols);
-        });
-    }
-    else {
-        this.push([]);
+    /**
+     * @method rowToCol 
+     * 
+     * @static
+     * @param {number[]} row 
+     * 
+     * @memberOf Matrix
+     */
+    static rowToCol(row: number[]): number[][] {
+        return row.map(num => [num]);
     }
 }
 
