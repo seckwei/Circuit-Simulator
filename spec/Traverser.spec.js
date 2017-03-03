d0a2aad7aace49019bcdf92ac15b392ea45f1521
Seck Wei <seckwei93@gmail.com>
2017-03-03 01:01:11 +0000

    Traverser tests almost done, need to review complete the final one as it might not be necessary to have it.

--- spec/Traverser.spec.js
+++ spec/Traverser.spec.js
@@ -1,61 +1,184 @@
-const TEST_DATA = require('./test_data.spec.js'),
+const VoltageSource = require('../src/VoltageSource.js'),
+    CurrentSource = require('../src/CurrentSource.js'),
+    Resistor = require('../src/Resistor.js'),
+    Wire = require('../src/Wire.js'),
+    Ground = require('../src/Ground.js'),
+    ComponentType = require('../src/ComponentType.js');
+
+const { WORKING_CIRCUIT } = require('./test_data.spec.js'),
     Traverser = require('../src/Traverser.js');
 
 describe('Traverser Module', () => {
 
-    describe('clearVisited(Board.Component)', () => {
+    describe('resetVisited(Board.components)', () => {
         it('should reset all \'visited\' flag of the pins to false', () => {
-            fail();
+            // Copy the object before we mutate it for testing
+            const components = Object.assign({}, WORKING_CIRCUIT.components);
+
+            // Set some pins visited to true
+            for(let key in components){
+                components[key][0].visited = true;
+            }
+
+            // Then, we reset them
+            Traverser.resetVisited(components);
+
+            // Finally, test
+            let flag = true;
+            for(let key in components){
+                if(!components[key].every(pin => pin.visited)){
+                    flag = false;
+                    break;
+                }
+            }
+            expect(flag).toBe(true);
         });
     });
 
-    describe('assignNode(component, pinIndex, nodes, nodeIndex)', () => {
-        it('should return updated array with newly assigned component and pin index in the given node index', () => {
-            fail();
+    describe('assignNode(pin, nodes, nodeIndex)', () => {
+        it('should update array with newly assigned pin in the given node index', () => {
+            let nodes = [],
+                nodeIndex = 1,
+                R1 = new Resistor(),
+                R2 = new Resistor();
+
+            Traverser.assignNode(R1.pins[0], nodes, nodeIndex);
+            Traverser.assignNode(R2.pins[0], nodes, nodeIndex);
+            expect(nodes[nodeIndex]).toEqual([R1.pins[0], R1.pins[0]]);
+
+            nodeIndex = 3;
+            Traverser.assignNode(R1.pins[1], nodes, nodeIndex);
+            Traverser.assignNode(R2.pins[1], nodes, nodeIndex);
+            expect(nodes[nodeIndex]).toEqual([R1.pins[1], R1.pins[1]]);
         });
     });
 
-    describe('flagVisited(component, pinIndex)', () => {
+    describe('flagVisited(pin)', () => {
         it('should change the \'visited\' field of that pin to true', () => {
-            fail();
+            let R1 = new Resistor();
+
+            Traverser.flagVisited(R1.pins[0]);
+            expect(R1.pins[0].visited).toBe(true);
+
+            Traverser.flagVisited(R1.pins[1]);
+            expect(R1.pins[1].visited).toBe(true);
         });
     });
 
-    describe('recordGndInd(component, GndIndex, nodeIndex)', () => {
-        it('should return updated array with newly added node index if the component is GND', () => {
-            fail();
+    describe('recordGndInd(pin, groundNodeIndices, nodeIndex)', () => {
+
+        let nodes,
+            nodeIndex,
+            groundNodeIndices,
+            GND;
+
+        beforeEach(() => {
+            nodes = [],
+            nodeIndex = 1,
+            groundNodeIndices = [];
+            GND = new Ground();
         });
 
-        it('should return same array if component is not GND', () => {
-            fail();
+        it('should update groundNodeIndices array with the corresponding node index if the component is GND', () => {
+            Traverser.recordGndInd(GND.pins[0], groundNodeIndices, nodeIndex);
+            expect(groundNodeIndices).toEquals([nodeIndex]);
+        });
+
+        it('should not update groundNodeIndices array with the same node index', () => {
+            Traverser.recordGndInd(GND.pins[0], groundNodeIndices, nodeIndex);
+            Traverser.recordGndInd(GND.pins[0], groundNodeIndices, nodeIndex);
+            expect(groundNodeIndices).toEquals([nodeIndex]);
+        });
+
+        it('should not update the groundNodeIndices array if component is not GND', () => {
+            let R1 = new Resistor();
+
+            Traverser.recordGndInd(R1.pins[0], groundNodeIndices, nodeIndex);
+            expect(groundNodeIndices).toEquals([]);
         });
     });
 
-    describe('enqueuePin(component, pinIndex)', () => {
-        it('should return update array with the component\'s other pin', () => {
-            fail();
+    describe('enqueuePin(pin, queue)', () => {
+        it('should update array with the component\'s other pin, provided if it has not been visited', () => {
+            let queue = [],
+                R1 = new Resistor(),
+                R2 = new Resistor();
+
+            Traverser.enqueue(R1.pins[0], queue);
+            expect(queue).toEqual([R1.pins[1]]);
+
+            R2.pins[1].visited = true;
+            Traverser.enqueue(R2.pins[0], queue);
+
+            // R2's pin should not be enqueued, because it was visited already
+            expect(queue).toEqual([R1.pins[1]]); 
         });
 
-        it('should return same array if all other component pins have been visited', () => {
-            fail();
+        it('should update array with the component\'s other pin, provided if it is not a GND', () => {
+            let queue = [],
+                R1 = new Resistor();
+                GND = new Ground();
+
+            Traverser.enqueue(R1.pins[0], queue);
+            Traverser.enqueue(GND.pins[0], queue);
+            Traverser.enqueue(GND.pins[1], queue);
+
+            expect(queue).toEqual([R1.pins[1]]);
         });
     });
 
-    describe('getNodes(Board.Component)', () => {
+    describe('getNodes(Board.components)', () => {
         it('should return a 2D array, cells may contain { Component, pinIndex }', () => {
-            fail();
+            /**
+             * We are going to compare equality of 2D array of objects, so what I have done here
+             * is to convert each Pin object into a string of ComponentID + PinIndex. Then, I sort
+             * the inner arrays first and finally the outer array itself. I assume The outer array
+             * auto sorts by comparing the inner array's first element.
+             */
+            let sortedActual = Traverser.getNodes(Object.assign({}, WORKING_CIRCUIT.components));
+            sortedActual = sortedActual.map(node => node.map(pin => pin.parent.id + pin.index).sort()).sort();
+
+            let sortedExpected = WORKING_CIRCUIT.nodes_prearranged.slice();
+            sortedExpected = sortedExpected.map(node => node.map(pin => pin.parent.id + pin.index).sort()).sort();
+
+            expect(sortedActual).toEquals(sortedExpected);
         });
     });
 
     describe('rearrange(nodes)', () => {
         it('should return arranged array nodes - rows with GND moved to row-0, and emptied rows are removed', () => {
-            fail();
+            let prearrangedNodes = Traverser.getNodes(WORKING_CIRCUIT.nodes_prearranged.slice())),
+                arrangedNodes = Traverser.rearrange(prearrangedNodes);
+
+            // Quick check - length should be same
+            expect(arrangedNodes.length).toBe(WORKING_CIRCUIT.nodes_arranged.length);
+
+            // Only Node 0 should have all the Ground components pins
+            let groundOnlyInIndex0 = true;
+            for(let i = 1; i < arrangedNodes.length; ++i){
+                if(!arrangedNodes[i].every(pin => pin.parent.type !== ComponentType.TYPE_GROUND)) {
+                    groundOnlyInIndex0 = false;
+                    break;
+                }
+            }
+            expect(groundOnlyInIndex0).toBe(true);
+
+            // Finally we check if they have the same content
+            let sortedActual = arrangedNodes.map(node => node.map(pin => pin.parent.id + pin.index).sort()).sort();
+            let sortedExpected = WORKING_CIRCUIT.nodes_arranged.slice();
+            sortedExpected = sortedExpected.map(node => node.map(pin => pin.parent.id + pin.index).sort()).sort();
+
+            expect(sortedActual).toEquals(sortedExpected);
         });
     });
 
     describe('updateComponentNodes(nodes)', () => {
         it('should fill in the components \'nodes\' field with the corresponding node indices', () => {
-            fail();
+            let components = Object.assign({}, WORKING_CIRCUIT.components),
+                nodes = Traverser.getNodes(components);
+                nodes = Traverser.rearrange(nodes);
+
+            Traverser.updateComponentNodes(nodes);
         });
     });
 
--- spec/test_data.spec.js
+++ spec/test_data.spec.js
@@ -1,3 +1,130 @@
-const WORKING_CIRCUIT = {
+const VoltageSource = require('../src/VoltageSource.js'),
+    CurrentSource = require('../src/CurrentSource.js'),
+    Resistor = require('../src/Resistor.js'),
+    Wire = require('../src/Wire.js'),
+    Ground = require('../src/Ground.js'); 
 
-}
+const I1 = new CurrentSource(5),
+    GND = new Ground(),
+    W1 = new Wire(),
+    W2 = new Wire(),
+    R1 = new Resistor(2),
+    R2 = new Resistor(4),
+    R3 = new Resistor(6),
+    R4 = new Resistor(8);
+
+const N0 = [0,10],
+    N1 = [ 0, 0],
+    N2 = [10, 0],
+    N3 = [20, 0],
+    N4 = [20,10],
+    N5 = [10,10];
+
+I1.place([N0, N1]);
+GND.place([N0, [0,20]]);
+W1.place([N1, N2]);
+R2.place([N2, N3]);
+R3.place([N3, N4]);
+W2.place([N4, N5]);
+R4.place([N5, N0]);
+R1.place([N2, N5]);
+
+const WORKING_CIRCUIT = {
+    components: {
+        [N0.toString()]: [
+            I1.pins[0],
+            GND.pins[0],
+            R4.pins[1]
+        ],
+
+        [N1.toString()]: [
+            I1.pins[1],
+            W1.pins[0]
+        ],
+
+        [N2.toString()]: [
+            W1.pins[1],
+            R1.pins[0],
+            R2.pins[0]
+        ],
+
+        [N3.toString()]: [
+            R2.pins[1],
+            R3.pins[0]
+        ],
+
+        [N4.toString()]: [
+            R3.pins[1],
+            W2.pins[0]
+        ],
+
+        [N5.toString()]: [
+            R1.pins[1],
+            W2.pins[1],
+            R4.pins[0]
+        ]
+    },
+
+    nodes_prearranged: [
+        [],
+        [
+            I1.pins[0],
+            GND.pins[0],
+            R4.pins[1]
+        ],
+        [
+            I1.pins[1],
+            W1.pins[0]
+        ],
+        [
+            W1.pins[1],
+            R1.pins[0],
+            R2.pins[0]
+        ],
+        [
+            R2.pins[1],
+            R3.pins[0]
+        ],
+        [
+            R3.pins[1],
+            W2.pins[0]
+        ],
+        [
+            R1.pins[1],
+            W2.pins[1],
+            R4.pins[0]
+        ]
+    ],
+
+    nodes_arranged: [
+        [
+            I1.pins[0],
+            GND.pins[0],
+            R4.pins[1]
+        ],
+        [
+            I1.pins[1],
+            W1.pins[0]
+        ],
+        [
+            W1.pins[1],
+            R1.pins[0],
+            R2.pins[0]
+        ],
+        [
+            R2.pins[1],
+            R3.pins[0]
+        ],
+        [
+            R3.pins[1],
+            W2.pins[0]
+        ],
+        [
+            R1.pins[1],
+            W2.pins[1],
+            R4.pins[0]
+        ]
+    ]
+};
+
+module.exports.WORKING_CIRCUIT = WORKING_CIRCUIT;
